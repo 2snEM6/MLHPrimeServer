3c818ca4ae61ffcd9847f900b3199698357437ad
Cristian Pinto <cristian.pinto@sage.com>
2017-04-01 19:55:58 +0100

    should work

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask
+from flask import Flask, json
 from flask import request
 from flask import json
-from flask import Response
 from flask import make_response
-import requests as re
+import request as req
 
 app = Flask(__name__)
 
@@ -20,11 +19,37 @@ def hello():
 
 @app.route("/webhook", methods=['POST'])
 def handle():
-    data = {
-        "speech": 'hola',
-        "displayText": 'holadios',
-    }
-    js = json.dumps(data)
+
+    response_data = {"displayText": 'this is text displayed'}
+    speech = ""
+    if request.method == 'POST' and request.headers['Content-Type'] == 'application/json':
+        body = json.dumps(request.json)
+        action = body['result']['action']
+
+        if action == 'SEARCH':
+            params = body['result']['parameters']
+            qs = []
+            for key in params:
+                if 'Param' in key:
+                    qs.append(params[key])
+            symbols = ''
+            symbol_first = True
+            for key in params:
+
+                if 'Symbol' in key:
+                    if symbol_first:
+                        symbol_first = False
+                        symbols = symbols + params[key]
+                    else:
+                        symbols = symbols + ',' + params[key]
+
+            resp = req.get(BASE_URL + GET_QUOTE, params={'key': KEY, 'symbols': symbols})
+
+            for param in params:
+                speech = speech + " the " + param + " is " + resp[param]
+
+    response_data["speech"] = speech
+    js = json.dumps(response_data)
     r = make_response(js)
     r.headers['Content-Type'] = 'application/json'
     return r
