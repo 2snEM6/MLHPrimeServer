9ea130f32aff47249de7aee5fee8abf9e9f0398e
Macbook <macbook@PC.local>
2017-04-01 16:33:18 +0100

    Bug fixes

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 from flask import Flask
+from flask import request
 import requests as re
+
 app = Flask(__name__)
 
-BASE_URL = "http://ondemand.websol.barchart.com/"
-KEY  "b1a585d027c8d89fa27ccd3628609739"
+BASE_URL = "http://marketdata.websol.barchart.com/"
+KEY = "b1a585d027c8d89fa27ccd3628609739"
 GET_QUOTE = "getQuote.json"
+SYMBOLS = "symbols"
 
-http://ondemand.websol.barchart.com/getQuote.json?symbols=AAPL%2CGOOG&fields=fiftyTwoWkHigh%2CfiftyTwoWkHighDate%2CfiftyTwoWkLow%2CfiftyTwoWkLowDate&mode=I
 
 @app.route("/")
 def hello():
     return 'Hello, world!'
 
+
 @app.route("/get_quote")
 def get_quote():
-    r = re.get(BASE_URL + GET_QUOTE, params=)
-
-
+    symbol = request.args.get('symbol')
+    r = re.get(BASE_URL + GET_QUOTE, params={'key': KEY, 'symbols': symbol})
+    return r.text
 
 
 if __name__ == '__main__':
